2133be10cc7033fc3a6f2fa2373860a2ee15a624
Kalimuthu <kalimuthu210@gmail.com>
2021-04-13 09:39:39 +0530

    new webpart viewjob

---
+++ src/webparts/viewJobDetails/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IViewJobDetailsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ViewJobDetailsWebPartStrings' {
+  const strings: IViewJobDetailsWebPartStrings;
+  export = strings;
+}
